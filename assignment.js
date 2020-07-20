4474453c889b2c64ec0a0526181b8531605982bf
afnbd <afransheak987@gmil.com>
2020-07-20 15:06:45 +0600

    javascript assignment complete

---
+++ assignment.js
@@ -0,0 +1,18 @@
+
+function feetToMile(feet){
+    var mile = feet / 5280;
+    return mile;
+    
+}
+
+
+function woodCalculator(chair, table, bed){
+    var chairWoodNeed = chair *1;
+    var tableWoodNeed = table* 3;
+    var bedWoodNeed = bed * 5;
+    var totalWoodNeed = chairWoodNeed + tableWoodNeed + bedWoodNeed;
+    return totalWoodNeed;
+}
+
+
+
